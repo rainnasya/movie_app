f2f7591a42551afd4a63d4595d1a0f292a2edc52
Rainnasya <rainnasyaaca@gmail.com>
2024-06-26 00:06:37 +0700

    develop home screen layout

--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -1,10 +1,42 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
+import { API_URL, API_ACCESS_TOKEN } from '@env'
 
-export default function Movie(): JSX.Element {
+const MovieDetail = ({ navigation }: any): any => {
+  const fetchData = (): void => {
+    if (API_URL == null || API_ACCESS_TOKEN.length == null) {
+        throw new Error('ENV not found')
+    }
+
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_ACCESS_TOKEN}`,
+      },
+    }
+
+    fetch(API_URL, options)
+      .then(async (response) => await response.json())
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+
   return (
-    <View>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Movie Detail Page</Text>
+      <Button
+        title="Fetch Data"
+        onPress={() => {
+          fetchData()
+        }}
+      />
     </View>
   )
 }
+
+export default MovieDetail
